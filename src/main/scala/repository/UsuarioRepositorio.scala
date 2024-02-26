3f02c9b68e1bab47df2cfef7ba6fc2e99b44e5e6
eacodemo <deyner700@gmail.com>
2024-02-26 13:57:02 -0500

    Repositorio cambio nombre para modelo Usuario

--- src/main/scala/repository/UsuarioRepositorio.scala
+++ src/main/scala/repository/UsuarioRepositorio.scala
@@ -3,7 +3,7 @@ package repository
 import doobie._
 import doobie.implicits._
 import model._
-class TodoRepository {
+class UsuarioRepositorio {
   def insert(usuario: Usuario): ConnectionIO[Int] =
     sql"INSERT INTO Usuario (ID, nombreUsuario, email, contraseña, tipo) VALUES (${usuario.ID}, ${usuario.nombreUsuario}, ${usuario.email}, ${usuario.contraseña}, ${usuario.tipo})"
       .update
--- src/main/scala/servicios/Api_restRoutes.scala
+++ src/main/scala/servicios/Api_restRoutes.scala
@@ -9,14 +9,14 @@ import doobie.implicits.toConnectionIOOps
 import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import io.circe.generic.auto._
 import model.Usuario
-import repository.TodoRepository
+import repository.UsuarioRepositorio
 
 object UsuarioRoutes {
   def routes(transactor: HikariTransactor[IO]): HttpRoutes[IO] =
     HttpRoutes.of[IO] {
       case req @ POST -> Root / "usuarios" =>
         req.decode[Usuario] { usuario =>
-          TodoRepository().insert(usuario).transact(transactor).attempt.flatMap {
+          UsuarioRepositorio().insert(usuario).transact(transactor).attempt.flatMap {
             case Right(1) => Ok("Usuario insertado correctamente")
             case Right(_) => InternalServerError("Error al insertar el usuario: No se pudo determinar el número de filas afectadas")
             case Left(ex) => InternalServerError(s"Error al insertar el usuario: ${ex.getMessage}")
