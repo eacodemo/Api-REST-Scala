cf53899124d67acea5914498c78c9e2605dc63bb
eacodemo <eacodemo@gmail.com>
2024-02-26 23:06:44 -0500

    Se agregaron mas queries para las tablas

--- src/main/scala/repository/UsuarioRepositorio.scala
+++ src/main/scala/repository/UsuarioRepositorio.scala
@@ -8,6 +8,26 @@ class UsuarioRepositorio {
     sql"INSERT INTO Usuario (ID, nombreUsuario, email, contraseña, tipo) VALUES (${usuario.ID}, ${usuario.nombreUsuario}, ${usuario.email}, ${usuario.contraseña}, ${usuario.tipo})"
       .update
       .run
+
+  def getAll: ConnectionIO[List[Usuario]] =
+    sql"SELECT ID, nombreUsuario, email, contraseña, tipo FROM Usuario"
+      .query[Usuario]
+      .to[List]
+
+  def getById(id: Int): ConnectionIO[Option[Usuario]] =
+    sql"SELECT ID, nombreUsuario, email, contraseña, tipo FROM Usuario WHERE ID = $id"
+      .query[Usuario]
+      .option
+
+  def update(usuario: Usuario): ConnectionIO[Int] =
+    sql"UPDATE Usuario SET nombreUsuario = ${usuario.nombreUsuario}, email = ${usuario.email}, contraseña = ${usuario.contraseña}, tipo = ${usuario.tipo} WHERE ID = ${usuario.ID}"
+      .update
+      .run
+
+  def deleteById(id: Int): ConnectionIO[Int] =
+    sql"DELETE FROM Usuario WHERE ID = $id"
+      .update
+      .run  
 }
 
 
