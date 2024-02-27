0740ceb4a554f59e3a1ea5ed7325d9306c142fb5
eacodemo <eacodemo@gmail.com>
2024-02-26 22:40:26 -0500

    Se agregaron los nuevos modelos faltantes

--- src/main/scala/model/modelos.scala
+++ src/main/scala/model/modelos.scala
@@ -4,3 +4,6 @@ import java.sql.Date
 
 case class Usuario(ID: Int, nombreUsuario: String, email: String, contraseña: String, tipo: String)
 case class Proyecto(ID: Int, nombre: String, descripcion: Option[String], fechaInicio: Option[Date])
+case class AsociacionProyectoUsuario(ID: Int, IDProyecto: Int, IDUsuario: Int, Rol: String)
+case class HistoriaUsuario(ID: Int, detalles: String, criteriosAceptacion: String, estado: String, IDProyecto: Int, IDTareaAsociada: Option[Int])
+case class Tarea(ID: Int, descripcion: String, estado: String, IDHistoriaUsuario: Int)
